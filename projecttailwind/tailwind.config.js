5da26111e0ea2780624d30b315deeebcd3f868e0
codinggirl2002 <ivanaazanfack@gmail.com>
2024-12-16 21:34:24 +0100

    new project

--- projecttailwind/tailwind.config.js
+++ projecttailwind/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./public/index.html",
             "./public/page1.html",
             "./public/page2.html",
+            "./public/page3.html",
            "./src/**/*.{html,js}"],
   theme: {
     extend: {
@@ -12,6 +13,8 @@ module.exports = {
       colors: {
         primary: '#10b981',
         secondary: '#f59e0b',
+        primary3:  '#f59e0b',
+        secondary3: '#f8fafc',
       },
     },
   },
